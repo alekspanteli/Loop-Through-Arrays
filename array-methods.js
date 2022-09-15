0086fc02d8e8f6085c3fdccdd3e4c0893669c563
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-09-15 09:47:24 +0300

    1 file modified, 2 added

---
+++ array-methods.js
@@ -0,0 +1,13 @@
+const myArray = ['test', 'test2', 'test3', 'test4', 'test5']
+
+const myArray2 = ['one', 'one2', 'one3', 'one4', 'one5']
+
+
+
+// console.log(myArray.join(' ')) // Outputs array items with space between them
+
+
+// console.log(myArray.includes('test23')) // Returns true or false if item in the array exists
+
+
+// console.log(myArray.indexOf('test2')) // Returns index value
